13a3f7ba8b13effa17afe7624e719f46e62ed1f0
nargiza-frontend <e.nargiza0592@gmail.com>
2023-01-19 15:51:09 +0500

    Add new js

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
-alert("Iltimos ma'lumotlaringizni kiriting!")
-var firstName=prompt("First-name:");
-var lastName=prompt("Last-name:");
-var fullName=prompt("Full-name:");
-var age=prompt("Age:");
-var address=prompt("Address:");
-var email=prompt("Email:");
-var phone=prompt("Phone-number:");
+// alert("Iltimos ma'lumotlaringizni kiriting!")
+// var firstName=prompt("First-name:");
+// var lastName=prompt("Last-name:");
+// var fullName=prompt("Full-name:");
+// var age=prompt("Age:");
+// var address=prompt("Address:");
+// var email=prompt("Email:");
+// var phone=prompt("Phone-number:");
 
-document.getElementById("first-name").innerHTML = firstName;
-document.getElementById("last-name").innerHTML = lastName;
-document.getElementById("full-name").innerHTML = fullName;
-document.getElementById("age").innerHTML = age;
-document.getElementById("address").innerHTML = address;
-document.getElementById("email").innerHTML = email;
-document.getElementById("phone-number").innerHTML = phone;
-document.getElementById("about-your").innerHTML = `${lastName} ${firstName}  ${fullName}1992-yilda Quva tumanida tug'ilgan. hozir ${age} yoshda ${address}ida yashaydi. Murojaat uchun: ${email} ${phone}`
+// document.getElementById("first-name").innerHTML = firstName;
+// document.getElementById("last-name").innerHTML = lastName;
+// document.getElementById("full-name").innerHTML = fullName;
+// document.getElementById("age").innerHTML = age;
+// document.getElementById("address").innerHTML = address;
+// document.getElementById("email").innerHTML = email;
+// document.getElementById("phone-number").innerHTML = phone;
+// document.getElementById("about-your").innerHTML = `${lastName} ${firstName}  ${fullName}1992-yilda Quva tumanida tug'ilgan. hozir ${age} yoshda ${address}ida yashaydi. Murojaat uchun: ${email} ${phone}`
+const eltext="Good Bye"
+const elSiteHeading=document.querySelector('.site-heading');
+const elDarkMode=document.querySelector('.dark-mode')
+const elBodyColorBtn=document.querySelector('.body-color-btn');
+const elSiteBtn=document.querySelector('.site-btn');
+
+elBodyColorBtn.addEventListener('click',function(){
+  document.body.classList.toggle('dark-mode');
+  elBodyColorBtn.textContent="This is black body"
+})
+elSiteBtn.addEventListener('click',function(){
+  elSiteHeading.textContent = eltext;
+  elSiteBtn.textContent="Hello"
+  document.elSiteBtn.toggle(eltext)
+})
